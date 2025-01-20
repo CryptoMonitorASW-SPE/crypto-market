d5d8309c39bca78af42f9d1d9678549ac2b3ad19
Alessandro Becci <trofui@gmail.com>
2025-01-20 16:37:43 +0100

    build: add klint support to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
 
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
@@ -37,9 +39,6 @@ dependencies {
     testImplementation(libs.junit.jupiter.engine)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation(libs.guava)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
