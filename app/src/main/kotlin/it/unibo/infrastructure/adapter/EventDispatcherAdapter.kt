9c99c193949c8256696b38cc75e4e42f3a921cc1
Alessandro Becci <trofui@gmail.com>
2025-03-02 23:02:38 +0100

    refactor: empty space in EventDispatcherAdapter

--- app/src/main/kotlin/it/unibo/infrastructure/adapter/EventDispatcherAdapter.kt
+++ app/src/main/kotlin/it/unibo/infrastructure/adapter/EventDispatcherAdapter.kt
@@ -20,18 +20,15 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import org.slf4j.LoggerFactory
 
-
-
 class EventDispatcherAdapter(
     private val httpServerHost: String = System.getenv("EVENT_DISPATCHER_SERVICE_NAME") ?: "event-dispatcher",
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO),
-    ) : EventDispatcher {
-
+) : EventDispatcher {
     companion object {
         private const val DEFAULT_PORT = 3000
     }
 
-    private val httpServerPort: Int = System.getenv("EVENT_DISPATCHER_SERVICE_PORT")?.toIntOrNull()?: DEFAULT_PORT
+    private val httpServerPort: Int = System.getenv("EVENT_DISPATCHER_SERVICE_PORT")?.toIntOrNull() ?: DEFAULT_PORT
     private val logger = LoggerFactory.getLogger(EventDispatcherAdapter::class.java)
     private val client = HttpClient(CIO)
     private val mutex = Mutex()
